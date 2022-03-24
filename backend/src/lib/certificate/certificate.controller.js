a04cd7cd79e4b0a9699f6a82e65dbc11760bb5b6
Ahmet Polat <polat2000@gmail.com>
2022-03-24 21:03:45 +0100

    init certificate controller inclusive pdf response

--- backend/src/lib/certificate/certificate.controller.js
+++ backend/src/lib/certificate/certificate.controller.js
@@ -1,5 +1,6 @@
-const certificateService = require("./certificate.service");
 const generalService = require("../general/general.service");
+const { DICTIONARY } = require("../general/general.models");
+const PDFDocument = require("pdfkit");
 
 const {
   User,
@@ -14,16 +15,101 @@ const generateCertificateAction = async (req, res) => {
   const userId = req.body.user_id;
   const organizationId = req.body.organization_id;
 
-  const allEvents = await generalService
-    .selectAllEventsByIssuerAndTarget({
+  const allEvents = (
+    await generalService.selectAllEventsByIssuerAndTarget({
       issuerId: userId,
       targetId: organizationId,
     })
-    .map((e) => new Event(e));
+  ).map((e) => new Event(e));
 
-  console.log({ allEvents });
+  const organization = new Organization(
+    await generalService.selectOrganizationById({ organizationId })
+  );
 
-  // TODO: here comes the business logic. What exactly are we going to consider?
+  const organizationTags = await generalService.selectAllTagsOfOrganization({
+    organizationId,
+  });
+
+  // Create a document
+  const doc = new PDFDocument({ bufferPages: true });
+
+  let buffers = [];
+
+  doc.on("data", buffers.push.bind(buffers));
+  doc.on("end", () => {
+    let pdfData = Buffer.concat(buffers);
+    res
+      .writeHead(200, {
+        "Content-Length": Buffer.byteLength(pdfData),
+        "Content-Type": "application/pdf",
+        "Content-disposition": "attachment;filename=test.pdf",
+      })
+      .end(pdfData);
+  });
+
+  // If not commented out, the next line saves the pdf in the file system.
+  // doc.pipe(fs.createWriteStream("output.pdf"));
+
+  // Embed a font, set the font size, and render some text
+  doc.font("static/PalatinoBold.ttf");
+  doc.fontSize(25);
+  doc.text(`Certificate of experience at`, 50, 50, { align: "center" });
+  doc.fontSize(12);
+  doc.moveDown().font("static/PalatinoBoldItalic.ttf");
+  doc.fontSize(25);
+  doc.text(organization.name, { align: "center" });
+
+  // Add an image, constrain it to a given size, and center it vertically and horizontally
+  doc.image("static/logo.png", 487.28, 663.89, {
+    fit: [128, 128],
+  });
+
+  doc.image("static/switchLogo.png", 10, 728.89, {
+    fit: [226.5, 57],
+  });
+
+  doc.fontSize(16);
+  doc.moveDown();
+  doc.moveDown();
+  doc.moveDown();
+
+  for (const event of allEvents) {
+    console.log({ timestamp: event.timestamp });
+    if (
+      event.type !== DICTIONARY.eventTypes.startSuspend &&
+      event.type !== DICTIONARY.eventTypes.stopSuspend
+    ) {
+      doc.text(DICTIONARY.prettyEventTypes[event.type], {
+        align: "left",
+      });
+      if (event.type === DICTIONARY.eventTypes.newRole) {
+        doc.text(
+          `${event.payload.role} at ${event.payload.department} deparment`,
+          {
+            align: "left",
+          }
+        );
+      }
+      doc.text(new Date(Number(event.timestamp)).toLocaleDateString(), {
+        align: "right",
+      });
+      doc.moveDown();
+      doc.moveDown();
+    }
+  }
+
+  // Finalize PDF file
+  doc.end();
 };
 
-module.exports = { generateCertificateAction };
+const listOrganizationsAction = async (req, res) => {
+  const userId = req.body.user_id;
+
+  const allOrganizations = await generalService.selectAllOrganizationsByUserId({
+    userId,
+  });
+
+  res.json({ msg: "ok", data: allOrganizations });
+};
+
+module.exports = { generateCertificateAction, listOrganizationsAction };
