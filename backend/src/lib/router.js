0a373e135e2fc1fa17bd0f055cb80689e4d24091
Ahmet Polat <polat2000@gmail.com>
2022-03-24 21:11:14 +0100

    rename block: eduID -> auth

--- backend/src/lib/router.js
+++ backend/src/lib/router.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-// const eduIDRouter = require("./eduID/eduID.router");
+const authRouter = require("./auth/auth.router");
 const certificateRouter = require("./certificate/certificate.router");
 const cvRouter = require("./cv/cv.router");
 const analyticsRouter = require("./analytics/analytics.router");
 const actionRouter = require("./action/action.router");
 
-// router.use("/edu-id", eduIDRouter);
+router.use("/auth", authRouter);
 router.use("/certificate", certificateRouter);
 router.use("/cv", cvRouter);
 router.use("/analytics", analyticsRouter);
