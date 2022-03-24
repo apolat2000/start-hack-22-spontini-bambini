b9aa27dcadc1e176ea93643f96776ab911f494b4
Ahmet Polat <polat2000@gmail.com>
2022-03-24 21:02:58 +0100

    init certificate router

--- backend/src/lib/certificate/certificate.router.js
+++ backend/src/lib/certificate/certificate.router.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { generateCertificateAction } = require("./certificate.controller");
+const {
+  generateCertificateAction,
+  listOrganizationsAction,
+} = require("./certificate.controller");
 
 router.get("/", (req, res) => {
   res.json({ msg: "Subroute works" });
 });
 
 router.get("/generate", generateCertificateAction);
+router.get("/list-organizations", listOrganizationsAction);
 
 module.exports = router;
