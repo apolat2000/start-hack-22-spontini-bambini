54ab6d3c19e7414504b6952d6a1620154f5c3d94
Arthur Becker <arthur.becker.private@gmail.com>
2022-03-24 15:41:15 +0100

    Connect AuthCubit with the frontend

--- frontend/lib/backend/auth/auth_cubit.dart
+++ frontend/lib/backend/auth/auth_cubit.dart
@@ -5,4 +5,15 @@ part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(UnauthenticatedState());
+
+  void openEduIDLogin() {
+    // TODO: implement login with eduID
+
+    // pretend, that user is logged in
+    emit(AuthenticatedState("{blablabla}"));
+  }
+
+  void logOut() {
+    emit(UnauthenticatedState());
+  }
 }
--- frontend/lib/frontend/pages/LoginPage.dart
+++ frontend/lib/frontend/pages/LoginPage.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/backend/auth/auth_cubit.dart';
+import 'package:frontend/frontend/components/ShadowBox.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("Login page"));
+    return Scaffold(
+        body: SafeArea(
+            child: Center(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        height: double.infinity,
+        constraints: BoxConstraints(maxWidth: 300),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const AspectRatio(
+              aspectRatio: 2,
+              child: Placeholder(),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            ElevatedButton(
+                onPressed: BlocProvider.of<AuthCubit>(context).openEduIDLogin,
+                child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: const Text(
+                    "Login with eduID",
+                  ),
+                ))
+          ],
+        ),
+      ),
+    )));
   }
 }
