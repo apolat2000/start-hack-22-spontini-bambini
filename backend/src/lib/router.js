ed7e99b668cf05e6c371d7ae8518b9b546369888
Ahmet Polat <polat2000@gmail.com>
2022-03-24 21:01:19 +0100

    init action block in the main router

--- backend/src/lib/router.js
+++ backend/src/lib/router.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const certificateRouter = require("./certificate/certificate.router");
 const cvRouter = require("./cv/cv.router");
 const analyticsRouter = require("./analytics/analytics.router");
+const actionRouter = require("./action/action.router");
 
 // router.use("/edu-id", eduIDRouter);
 router.use("/certificate", certificateRouter);
 router.use("/cv", cvRouter);
 router.use("/analytics", analyticsRouter);
+router.use("/action", actionRouter);
 
 module.exports = router;
